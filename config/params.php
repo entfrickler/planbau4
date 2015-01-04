66ee9d172f42cb8c6e80d93cf87e97eb3542a80b
Your Name <you@example.com>
2015-01-04 23:26:48 +0100

    fixes error in config

--- config/params.php
+++ config/params.php
@@ -1,6 +1,6 @@
 <?php
 
 return [
-    'companyName'=>'kontakt - p l a n b a u 4 | architekten . ingenieure'
+    'companyName'=>'kontakt - p l a n b a u 4 | architekten . ingenieure',
     'adminEmail' => 'admin@example.com',
 ];
